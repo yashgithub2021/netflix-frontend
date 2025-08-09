2578526f8576fe6b319fd87a4056f328ac23f366
yashgithub2021 <yashbarge007@gmail.com>
2025-08-09 14:56:19 +0530

    backend baseUrl changes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,5 +1,5 @@
 export const variables = {
-    BACKEND_URL: "http://localhost:4000/api/v1",
+    BACKEND_URL: "https://netflix-backend-fybj.onrender.com/api/v1",
     MOVIE_CATEGORIES: ['upcoming', 'now_playing', 'popular', 'top_rated'],
     TV_CATEGORIES: ['airing_today', 'on_the_air', 'popular', 'top_rated'],
     SMALL_IMG_BASE_URL: "https://image.tmdb.org/t/p/w500"
